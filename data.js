72a49d57250bcd78d39ad39c577fcd95ddc44dc2
Janjan Medina <medinajuanantonio95@gmail.com>
2019-05-01 17:12:49 +0800

    Fix scoring

---
+++ data.js
@@ -0,0 +1,90 @@
+const teams = [
+    {
+        image: '../assets/alphabetical.png',
+        name: 'College of Science',
+        shortcut: 'SCIENCE',
+        score: 0,
+        total: 0
+    },
+    {
+        image: '../assets/colleges/iics.png',
+        name: 'Institute of Information and Computing Sciences',
+        shortcut: 'IICS',
+        score: 0,
+        total: 0
+    },
+    {
+        image: '../assets/alphabetical.png',
+        name: 'Faculty of Arts and Letters',
+        shortcut: 'AB',
+        score: 0,
+        total: 0
+    },
+    {
+        image: '../assets/alphabetical.png',
+        name: 'College of Nursing',
+        shortcut: 'NURSING',
+        score: 0,
+        total: 0
+    },
+    {
+        image: '../assets/colleges/educ.png',
+        name: 'College of Education',
+        shortcut: 'EDUC',
+        score: 0,
+        total: 0
+    },
+    {
+        image: '../assets/alphabetical.png',
+        name: 'College of Tourism and Hospitality',
+        shortcut: 'CTHM',
+        score: 0,
+        total: 0
+    },
+    {
+        image: '../assets/alphabetical.png',
+        name: 'Institute of Physical Education and Athletics',
+        shortcut: 'IPEA',
+        score: 0,
+        total: 0
+    },
+    {
+        image: '../assets/alphabetical.png',
+        name: 'College of Accountancy',
+        shortcut: 'AMV',
+        score: 0,
+        total: 0
+    },
+    {
+        image: '../assets/colleges/music.png',
+        name: 'College of Music',
+        shortcut: 'MUSIC',
+        score: 0,
+        total: 0
+    },
+    {
+        image: '../assets/alphabetical.png',
+        name: 'Faculty of Pharmacy',
+        shortcut: 'PHARMA',
+        score: 0,
+        total: 0
+    },
+    {
+        image: '../assets/alphabetical.png',
+        name: 'College of Engineering',
+        shortcut: 'ENGG',
+        score: 0,
+        total: 0
+    },
+    {
+        image: '../assets/alphabetical.png',
+        name: 'College of Commerce and Business Administration',
+        shortcut: 'COMM',
+        score: 0,
+        total: 0
+    }
+]
+
+module.exports = {
+    teams
+}
